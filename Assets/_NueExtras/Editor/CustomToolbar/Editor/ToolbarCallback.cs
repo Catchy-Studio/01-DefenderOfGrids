d2b742464adf0018b59256b16c6f242584045958
Arif Mertcan Kara <arifmertcankara@gmail.com>
2026-02-13 17:27:01 +0300

    fix. Toolbar

--- Assets/_NueExtras/Editor/CustomToolbar/Editor/ToolbarCallback.cs
+++ Assets/_NueExtras/Editor/CustomToolbar/Editor/ToolbarCallback.cs
@@ -2,37 +2,16 @@ using System;
 using UnityEngine;
 using UnityEditor;
 using System.Reflection;
-
-#if UNITY_2019_1_OR_NEWER
 using UnityEngine.UIElements;
-#else
-using UnityEngine.Experimental.UIElements;
-#endif
 
 namespace UnityToolbarExtender
 {
 	public static class ToolbarCallback
 	{
-		static Type m_toolbarType = typeof(Editor).Assembly.GetType("UnityEditor.Toolbar");
-		static Type m_guiViewType = typeof(Editor).Assembly.GetType("UnityEditor.GUIView");
-#if UNITY_2020_1_OR_NEWER
-		static Type m_iWindowBackendType = typeof(Editor).Assembly.GetType("UnityEditor.IWindowBackend");
-		static PropertyInfo m_windowBackend = m_guiViewType.GetProperty("windowBackend",
-			BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-		static PropertyInfo m_viewVisualTree = m_iWindowBackendType.GetProperty("visualTree",
-			BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-#else
-		static PropertyInfo m_viewVisualTree = m_guiViewType.GetProperty("visualTree",
-			BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-#endif
-		static FieldInfo m_imguiContainerOnGui = typeof(IMGUIContainer).GetField("m_OnGUIHandler",
-			BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-		static ScriptableObject m_currentToolbar;
+		private static readonly Type ToolbarType = typeof(Editor).Assembly.GetType("UnityEditor.Toolbar");
+		private static ScriptableObject _currentToolbar;
+		private static bool _initialized;
 
-		/// <summary>
-		/// Callback for toolbar OnGUI method.
-		/// </summary>
-		public static Action OnToolbarGUI;
 		public static Action OnToolbarGUILeft;
 		public static Action OnToolbarGUIRight;
 		
@@ -44,68 +23,94 @@ namespace UnityToolbarExtender
 
 		static void OnUpdate()
 		{
-			// Relying on the fact that toolbar is ScriptableObject and gets deleted when layout changes
-			if (m_currentToolbar == null)
+			if (_currentToolbar == null)
 			{
-				// Find toolbar
-				var toolbars = Resources.FindObjectsOfTypeAll(m_toolbarType);
-				m_currentToolbar = toolbars.Length > 0 ? (ScriptableObject) toolbars[0] : null;
-				if (m_currentToolbar != null)
-				{ 
-#if UNITY_2021_1_OR_NEWER
-					var root = m_currentToolbar.GetType().GetField("m_Root", BindingFlags.NonPublic | BindingFlags.Instance);
-					var rawRoot = root.GetValue(m_currentToolbar);
-					var mRoot = rawRoot as VisualElement;
-					RegisterCallback("ToolbarZoneLeftAlign", OnToolbarGUILeft);
-					RegisterCallback("ToolbarZoneRightAlign", OnToolbarGUIRight);
-
-					void RegisterCallback(string root, Action cb) {
-						var toolbarZone = mRoot.Q(root);
-
-						var parent = new VisualElement()
-						{
-							style = {
-								flexGrow = 1,
-								flexDirection = FlexDirection.Row,
-							}
-						};
-						var container = new IMGUIContainer();
-						container.style.flexGrow = 1;
-						container.onGUIHandler += () => { 
-							cb?.Invoke();
-						}; 
-						parent.Add(container);
-						toolbarZone.Add(parent);
-					}
-#else
-#if UNITY_2020_1_OR_NEWER
-					var windowBackend = m_windowBackend.GetValue(m_currentToolbar);
-
-					// Get it's visual tree
-					var visualTree = (VisualElement) m_viewVisualTree.GetValue(windowBackend, null);
-#else
-					// Get it's visual tree
-					var visualTree = (VisualElement) m_viewVisualTree.GetValue(m_currentToolbar, null);
-#endif
-
-					// Get first child which 'happens' to be toolbar IMGUIContainer
-					var container = (IMGUIContainer) visualTree[0];
-
-					// (Re)attach handler
-					var handler = (Action) m_imguiContainerOnGui.GetValue(container);
-					handler -= OnGUI;
-					handler += OnGUI;
-					m_imguiContainerOnGui.SetValue(container, handler);
+				var toolbars = Resources.FindObjectsOfTypeAll(ToolbarType);
+				_currentToolbar = toolbars.Length > 0 ? (ScriptableObject)toolbars[0] : null;
+				
+				if (_currentToolbar != null && !_initialized)
+				{
+					_initialized = true;
 					
-#endif
+					var rootField = _currentToolbar.GetType().GetField("m_Root", BindingFlags.NonPublic | BindingFlags.Instance);
+					if (rootField == null)
+					{
+						Debug.LogWarning("CustomToolbar: m_Root field not found");
+						return;
+					}
+					
+					var rawRoot = rootField.GetValue(_currentToolbar);
+					if (rawRoot == null)
+					{
+						Debug.LogWarning("CustomToolbar: m_Root value is null");
+						return;
+					}
+					
+					var root = rawRoot as VisualElement;
+					if (root == null)
+					{
+						Debug.LogWarning("CustomToolbar: Root is not a VisualElement");
+						return;
+					}
+
+					Debug.Log("CustomToolbar: Inspecting toolbar structure...");
+					LogVisualTreeRecursive(root, 0);
+
+					// Try multiple possible zone names for Unity 6.3+
+					var leftZone = root.Q("ToolbarZoneLeftAlign") 
+					            ?? root.Q("unity-toolbar-zone-left-align")
+					            ?? root.Q("ToolbarZoneLeft");
+					
+					var rightZone = root.Q("ToolbarZoneRightAlign") 
+					             ?? root.Q("unity-toolbar-zone-right-align")
+					             ?? root.Q("ToolbarZoneRight");
+					
+					if (leftZone != null)
+					{
+						AddToolbarCallback(leftZone, OnToolbarGUILeft);
+						Debug.Log("CustomToolbar: Left zone registered");
+					}
+					else
+					{
+						Debug.LogWarning("CustomToolbar: Left zone not found");
+					}
+					
+					if (rightZone != null)
+					{
+						AddToolbarCallback(rightZone, OnToolbarGUIRight);
+						Debug.Log("CustomToolbar: Right zone registered");
+					}
+					else
+					{
+						Debug.LogWarning("CustomToolbar: Right zone not found");
+					}
 				}
+			}
+			else if (_currentToolbar == null && _initialized)
+			{
+				// Toolbar was destroyed, reset for re-initialization
+				_initialized = false;
 			}
 		}
 
-		static void OnGUI()
+		private static void AddToolbarCallback(VisualElement zone, Action callback)
 		{
-			var handler = OnToolbarGUI;
-			if (handler != null) handler();
+			var container = new IMGUIContainer();
+			container.style.flexGrow = 1;
+			container.onGUIHandler += () => callback?.Invoke();
+			zone.Add(container);
+		}
+
+		private static void LogVisualTreeRecursive(VisualElement element, int depth)
+		{
+			var indent = new string(' ', depth * 2);
+			var name = string.IsNullOrEmpty(element.name) ? "<no name>" : element.name;
+			Debug.Log($"{indent}Element: {name} (Type: {element.GetType().Name})");
+			
+			foreach (var child in element.Children())
+			{
+				LogVisualTreeRecursive(child, depth + 1);
+			}
 		}
 	}
 }
